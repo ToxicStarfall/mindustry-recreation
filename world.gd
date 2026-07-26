03c1c26c525a353a1c15f0c5a9d68feb56523422
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-07-25 23:00:49 -0500

    Added camera panning
    pan w/ middle mouse

--- world.gd
+++ world.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 
+
 @onready var Camera = get_node("%Camera2D")
 var camera_locked: bool = false
+var camera_panning: bool = false
+var camera_pan_start: Vector2
+
 
 var blocks: Array[Block]
 var units: Array[Unit]
@@ -47,6 +51,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		Camera.zoom = (Camera.zoom + (Vector2.ONE * zoom_factor)).minf(2.5)
 	if event.is_action_pressed("zoom_out"):
 		Camera.zoom = (Camera.zoom - (Vector2.ONE * zoom_factor)).maxf(0.25)
+	RenderingServer.global_shader_parameter_set("zoom_level", Camera.zoom.length())
+	
+	if !Game.controlled_entity:
+		if event.is_action_pressed("pan_camera"):
+			camera_panning = true
+			camera_pan_start = get_global_mouse_position()
+	if event.is_action_released("pan_camera"):
+		camera_panning = false
 
 	#if !camera_locked:
 		#var pan_x = Input.get_axis("camera_pan_left", "camera_pan_right")
@@ -54,47 +66,35 @@ func _unhandled_input(event: InputEvent) -> void:
 		#camera_pan_velocity = Vector2(pan_x, pan_y).normalized() * (512 + 256)
 
 
-#func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	#if !camera_locked:
 		#Camera.position += camera_pan_velocity * delta
+	if camera_panning:
+		var camera_pan_offset: Vector2 = camera_pan_start - get_global_mouse_position()
+		Camera.position += camera_pan_offset
 
 
-func _on_camera_lock_changed():
-	get_viewport().gui_release_focus()
-	camera_locked = !camera_locked
-	Camera.limit_enabled = camera_locked
-	Camera.drag_horizontal_enabled = camera_locked
-	Camera.drag_vertical_enabled = camera_locked
+#func _on_camera_lock_changed():
+	#get_viewport().gui_release_focus()
+	#camera_locked = !camera_locked
+	#Camera.limit_enabled = camera_locked
+	#Camera.drag_horizontal_enabled = camera_locked
+	#Camera.drag_vertical_enabled = camera_locked
+#
+	#if camera_locked:
+		#Camera.position = Vector2.ZERO
+		#Camera.position_smoothing_speed = 5.0
+		##Camera.drag_top_margin = 0.2
+		##Camera.drag_bottom_margin = 0.2
+		##Camera.drag_left_margin = 0.2
+		##Camera.drag_right_margin = 0.2
+	#else:
+		#Camera.position_smoothing_speed = 10.0
+		##Camera.drag_top_margin = 0.05
+		##Camera.drag_bottom_margin = 0.05
+		##Camera.drag_left_margin = 0.05
+		##Camera.drag_right_margin = 0.05
 
-	if camera_locked:
-		Camera.position = Vector2.ZERO
-		Camera.position_smoothing_speed = 5.0
-		#Camera.drag_top_margin = 0.2
-		#Camera.drag_bottom_margin = 0.2
-		#Camera.drag_left_margin = 0.2
-		#Camera.drag_right_margin = 0.2
-	else:
-		Camera.position_smoothing_speed = 10.0
-		#Camera.drag_top_margin = 0.05
-		#Camera.drag_bottom_margin = 0.05
-		#Camera.drag_left_margin = 0.05
-		#Camera.drag_right_margin = 0.05
-
-
-func _on_child_entered_tree(node: Node):
-	if node is Entity:
-		node.mouse_entered.connect( _on_mouse_entered_entity.bind(node) )
-		node.mouse_exited.connect( _on_mouse_exited_entity.bind(node) )
-		if node is Unit:
-			units.append(node)
-		
-
-func _on_child_exiting_tree(node: Node):
-	if node is Entity:
-		node.mouse_entered.disconnect( _on_mouse_entered_entity )
-		node.mouse_exited.disconnect( _on_mouse_exited_entity )
-		if node is Unit:
-			units.erase(node)
 
 	
 func _on_projectile_spawn_requested(projectile: Projectile):

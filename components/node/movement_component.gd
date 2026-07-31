5751d2632925e617083c63df62960d9f2f26f5c4
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-07-31 17:56:23 -0500

    Update movement handling and visuals
    Split physical movement processing and movement visual effects.
    Created basic leg stride effect for bipedal movement.

--- components/node/movement_component.gd
+++ components/node/movement_component.gd
@@ -19,6 +19,10 @@ enum MovementType {
 var unit: Entity
 var dir: Vector2
 
+var physics_dalta: float = 0.0
+var process_delta: float = 0.0
+
+
 
 func _ready() -> void:
 	unit = get_parent()
@@ -39,23 +43,55 @@ func physics_process(delta: float) -> void:
 				unit.velocity = Vector2(0, y * (speed * 32)).rotated(unit.rotation)
 				unit.move_and_slide()
 				unit.rotation_degrees += x * rot_speed * delta
-				
-				unit.get_node("%Tracks").material.set_shader_parameter("dir_x", x)
-				unit.get_node("%Tracks").material.set_shader_parameter("dir_y", y)
-				#unit.get_node("Tracks").region_rect.position.y -= (48.0 / 64) * y
 				pass
 				
 			MovementType.LEGGED:
-				unit.velocity = Vector2(x, y) * speed * Game.TILE_SIZE
+				unit.velocity = dir * (speed * Game.TILE_SIZE)
 				unit.move_and_slide()
 				pass
 				
 			MovementType.WHEELED:
 				pass
 				
-			_:
-				#print("default")
+			MovementType.NONE, _:
 				pass
 
-#func _unhandled_input(_event: InputEvent) -> void:
-	#pass
+
+func _process(delta: float) -> void:
+	if can_move:
+		match movement_type:
+			MovementType.TRACKED:
+				unit.get_node("%Tracks").material.set_shader_parameter("dir_x", dir.x)
+				unit.get_node("%Tracks").material.set_shader_parameter("dir_y", dir.y)
+				#unit.get_node("Tracks").region_rect.position.y -= (48.0 / 64) * y
+				pass
+				
+			MovementType.LEGGED:
+				# NOTE - Legs scale and move on steps
+				var legs = unit.get_node("Legs").get_children()
+				var leg_count = legs.size()
+				var step_size = 10
+				var step_speed = 5
+				
+				if !dir.is_zero_approx():
+					process_delta += delta
+					var last = 0.0
+					
+					for i in legs.size():
+						var leg = legs[i]
+						
+						# Determines bipedal leg movement direction depending on leg index.
+						var a = (1 * -1) ** i
+						# Leg y pos follows sine movement pattern
+						leg.position.y = 0 + ( sin(process_delta * step_speed) * step_size ) * a
+						
+						# TODO - Scale leg.y from 1 <-> 0.5 as leg moves backwards and vice versa when forwards.
+						
+
+				pass
+				
+			MovementType.WHEELED:
+				pass
+			
+			MovementType.NONE, _:
+				pass

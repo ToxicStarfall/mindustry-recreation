032d5f298e0712c7dd3ff5334de682c5565f6bd9
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-08-23 22:32:25 -0500

    Laser projectile fixes
    - fix incorrect spawn angle
    - adjustments to laser spawn visuals

--- entities/projectiles/laser_projectile.gd
+++ entities/projectiles/laser_projectile.gd
@@ -4,36 +4,42 @@ extends Projectile
 
 
 @export var beam_length: float = 5.0  ## The length of the laser beam in tiles.
+@export var beam_width: float = 48.0  ## The width of the laser beam in pixels.
+@export var beam_blast_size: Vector2 = Vector2(32, 32)
+const beam_overlap: int = 8
+# TODO - Add laser end aoe area hitbox
+# Currently laser hitbox is limited to the length of the beam.
 
 @export var stops_on_collision: bool = false
+#@export var grows: bool = false
 
 
 func _ready() -> void:
 	if get_tree().edited_scene_root != self:
 		get_tree().create_timer(lifetime).timeout.connect( _on_lifetime_timeout )
-		#
-	#if speed > trail_threshold * Game.TILE_SIZE:
-		#$Trail.show()
 	
-	modulate.a = 0
-	$Line2D.width = 0
-	var tween: Tween = create_tween().set_parallel()
-	tween.tween_property(self, "modulate:a", 1, 0.25)
-	tween.tween_property($Line2D, "width", 12, 0.25)
+		modulate.a = 0
+		$Line2D.width = 0
+		var tween: Tween = create_tween().set_parallel()
+		tween.tween_property(self, "modulate:a", 1, 0.1)
+		tween.tween_property($Line2D, "width", 48, 0.1)
+		
+		$CollisionShape2D.shape.size = Vector2(beam_width / 2, beam_length * Game.TILE_SIZE + 16)
+		$CollisionShape2D.position = -Vector2(0, (beam_length * Game.TILE_SIZE + 16) / 2)
+
 
 
 func _process(_delta: float) -> void:
 	if get_tree().edited_scene_root != self:
 		# NOTE - Laser projectile is already rotated to correct direction.
 		# The Laser beam only needs to extend in default direction (-y dir).
-
-		#$RayCast2D.target_position = self.direction.rotated(-PI/2) * beam_length * Game.TILE_SIZE
-
-		$RayCast2D.target_position = self.direction * beam_length * Game.TILE_SIZE
-		$Line2D.set_point_position(0, $RayCast2D.position)
-		#$Line2D.set_point_position(1, $RayCast2D.target_position)
-		$Line2D.set_point_position(1, Vector2(0,-1) * beam_length * Game.TILE_SIZE)
-	pass
+		
+		#$RayCast2D.target_position = Vector2(0, -1) * beam_length * Game.TILE_SIZE
+		var target_position = Vector2(0, -1) * beam_length * Game.TILE_SIZE
+		
+		#$Line2D.set_point_position(0, $RayCast2D.position)
+		$Line2D.set_point_position(1, target_position + Vector2(0, 8))
+		$LaserEnd.position = target_position
 
 
 func _physics_process(_delta: float) -> void:
@@ -42,7 +48,7 @@ func _physics_process(_delta: float) -> void:
 
 func collided():
 	# Do things after having collided with an object
-	super()
+	#super()
 	pass
 
 
@@ -55,8 +61,8 @@ func _on_lifetime_timeout():
 	#for particle in despawn_particles:
 		#Events.particle_spawn_requested.emit(particle, self.global_position)
 	var tween: Tween = create_tween().set_parallel()
-	tween.tween_property(self, "modulate:a", 0, 0.25)
-	tween.tween_property($Line2D, "width", 0, 0.25)
+	tween.tween_property(self, "modulate:a", 0, 0.15)
+	tween.tween_property($Line2D, "width", 0, 0.15)
 	await tween.finished
 	queue_free()
 	pass
